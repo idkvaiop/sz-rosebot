974d8b784371f28fdd883b6cff75e075907d467c
idkvaiop <104100121+idkvaiop@users.noreply.github.com>
2022-09-08 19:48:02 +0530

    Update filters.py

--- Rose/plugins/filters.py
+++ Rose/plugins/filters.py
@@ -270,7 +270,7 @@ async def filters_watcher(c: app, m: Message):
 __MODULE__ = Filter
 __HELP__ = """
 Make your chat more lively with filters; The bot will reply to certain words!
-Filters are case insensitive; every time someone says your trigger words, Rose will reply something else! can be used to create your own commands, if desired.
+Filters are case insensitive; every time someone says your trigger words, Akira will reply something else! can be used to create your own commands, if desired.
 
 **Commands:**
 - /filter <trigger> <reply>: Every time someone says "trigger", the bot will reply with "sentence". For multiple word filters, quote the trigger.
