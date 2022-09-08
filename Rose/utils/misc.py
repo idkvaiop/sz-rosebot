26772f0bae53f1d118c4d46078ef4b0e5b364fd8
idkvaiop <104100121+idkvaiop@users.noreply.github.com>
2022-09-08 18:54:08 +0530

    Update misc.py

--- Rose/utils/misc.py
+++ Rose/utils/misc.py
@@ -96,14 +96,14 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
             ]
         )
 
-    pairs = [modules[i * 3 : (i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)]
+    pairs = [modules[i * 2 : (i + 1) * 2] for i in range((len(modules) + 3 - 1) // 3)]
 
-    round_num = len(modules) / 3
+    round_num = len(modules) / 2
     calc = len(modules) - round(round_num)
     if calc in [1, 2]:
         pairs.append((modules[-1],))
 
-    max_num_pages = ceil(len(pairs) / 4)
+    max_num_pages = ceil(len(pairs) / 5)
     modulo_page = page_n % max_num_pages
 
     # can only have a certain amount of buttons side by side
@@ -111,17 +111,17 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
         pairs = pairs[modulo_page * 4 : 4 * (modulo_page + 1)] + [
             (
                 EqInlineKeyboardButton(
-                    "◁", callback_data="{}_prev({})".format(prefix, modulo_page)
+                    "⫷", callback_data="{}_prev({})".format(prefix, modulo_page)
                 ),
-                EqInlineKeyboardButton("• ʜᴏᴍᴇ •", callback_data="fallen_back"),
+                EqInlineKeyboardButton("Home 🏠", callback_data="fallen_back"),
                 EqInlineKeyboardButton(
-                    "▷", callback_data="{}_next({})".format(prefix, modulo_page)
+                    "⫸", callback_data="{}_next({})".format(prefix, modulo_page)
                 ),
             )
         ]
 
     else:
-        pairs += [[EqInlineKeyboardButton("• ʙᴀᴄᴋ •", callback_data="fallen_back")]]
+        pairs += [[EqInlineKeyboardButton("⫷", callback_data="fallen_back")]]
 
     return pairs
 
